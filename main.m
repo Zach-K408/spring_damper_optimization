97ea331df1fd227a3ac54947841832a1d7a71f63
Zach KElly <zach.kelly408@gmail.com>
2023-05-25 22:04:11 +0200

    update

--- main.m
+++ main.m
@@ -4,8 +4,15 @@ clear
 close all;
 
 % parameters
+m = 100; % kg
+c = 2; % kg/s
+k = 15; % kg/s^2
 
-m = 100;
-c = 2;
-k = 15;
+%%write a function that computes the acceleration of the sprung mass when varying the decision
+%%variables, namely the stiffness of the spring and damper.
 
+function a = analysis(m,c,k,f)
+
+a = 1/m * (f - c*v - k*x)
+
+end
