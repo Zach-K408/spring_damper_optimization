6b0c62e78b9cfce66772a195c3103d926201d7a7
Zach KElly <zach.kelly408@gmail.com>
2023-06-30 22:51:30 +0200

    update

--- optimize.m
+++
@@ -1,9 +0,0 @@
-%% Optimization of Passive Suspension System %%
-
-% find optimal values of c and k for a given road function input. 
-
-% objective: Minimize vertical acceleration experienced by passenger
-
-% design variables: Car spring and damping coefficients
-
-% constraints: Car suspension travel and coefficient values (physical limits of tire position)
