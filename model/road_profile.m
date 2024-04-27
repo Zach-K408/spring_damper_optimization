c029437961fe287235a973ad9f1fb1554e4c1592
zachk <zach.kelly408@gmail.com>
2024-04-27 10:05:39 -0700

    cleaning

--- model/road_profile.m
+++
@@ -1,9 +0,0 @@
-function zr = road_profile(t, p)
-% quarter car model with trailing arm suspension on random road
-
-% pseudo random road input
-s = p.v*t; 
-u = sum(p.Amp.*sin(p.Om*s+p.Psi)); 
-zr = u;
-
-end
